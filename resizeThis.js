f7704ffa922c209d0bef60625d26f93187e23e76
Emmanuel <m4nu@hotmail.fr>
2014-07-18 19:16:58 +0800

    Adds Basic Resizability

--- resizeThis.js
+++ resizeThis.js
@@ -55,8 +55,57 @@
     ResizeThis.prototype.init = function () {
         this._supported = _supports( 'resize' );
 
+        // @TODO should leverage CSS Resize whenever possible
         this._supported && this.$el.addClass( 'rt-resizable' );
+
+        // register events
+        this.$el.on( 'mousedown.rtClick', $.proxy(this._mouseStart, this) );
     };
+
+    ResizeThis.prototype._mouseStart = function ( evt ) {
+        var width = this.$el.innerWidth();
+        var height = this.$el.innerWidth();
+        $( document ).on( 'mousemove.rtMove', $.proxy(this._mouseDrag, this) );
+        $( document ).on( 'mouseup.rtClick', $.proxy(this._mouseStop, this) );
+        this._startPos = {
+            x: evt.pageX,
+            y: evt.pageY,
+        }
+
+        this._startSize = {
+            x: width,
+            y: height
+        }
+
+        console.log('mouse down', this._startSize);
+        console.log('mouse down', this._startPos);
+    }
+
+    ResizeThis.prototype._mouseStop = function ( evt ) {
+        $( document ).off( 'mousemove.rtMove' );
+    }
+
+    ResizeThis.prototype._mouseDrag = function( evt ) {
+        console.log(evt);
+        
+         var XY = {
+            x: evt.pageX - this._startPos.x,
+            y: evt.pageY - this._startPos.y
+        }
+         this._resize( XY );
+        console.log('mouse drag', XY);
+
+    }
+
+    ResizeThis.prototype._resize = function( XY ) {
+        var sX = this._startSize.x;
+        var sY = this._startSize.y;
+
+        this.$el.css({
+            width: sX + XY.x,
+            height: sY + XY.y
+        })
+    }
 
     // A really lightweight plugin wrapper around the constructor,
     // preventing against multiple instantiations
--- tests/tests.js
+++ tests/tests.js
@@ -1,21 +1,49 @@
+var fix;
+
 QUnit.test( "Plugin namespace exists", function( assert ) {
     assert.ok( $('#idontexist').resizeThis );
 });
 
-var fix;
 QUnit.module( "CSS support", {
     setup: function() {
-        console.log('testoin');
         fix = createFix('div', 'resizeMe');
     },
     teardown: function() {
         removeFix( fix );
     }
 });
+
 QUnit.test( "Should add class resizable if CSS supports resizing", function( assert ) {
     document.body.style.rotate = true;
     $('#resizeMe').resizeThis();
     assert.ok( fix.className.indexOf('resizable') > -1 );
+});
+
+QUnit.module( "Resize Element", {
+    setup: function() {
+        fix = createFix('div', 'resizeMe');
+    },
+    teardown: function() {
+        removeFix( fix );
+        $( document ).mouseup();
+    }
+});
+
+QUnit.test( "Should change clicked element size on drag", function( assert ) {
+    var $el = $('#resizeMe').resizeThis();
+    var oW = $el.innerWidth();
+    var oH = $el.innerHeight();
+    var e = $.Event('mousedown');
+    e.pageX = 10;
+    e.pageY = 10;
+    $el.trigger( e );
+
+    var e2 = $.Event('mousemove');
+    e2.pageX = 100;
+    e2.pageY = 100;
+    $( document ).trigger( e2 );
+
+    assert.ok( oW !== $el.innerWidth() );
 });
 
 
